89717bd0fdecf44efebd868c2b9a149d7d7d68ca
chasemoore <xflattopx@gmail.com>
2024-09-19 14:46:23 -0400

    Test class skeleton setup

---
+++ src/main/java/assignment01/BankAccount.java
@@ -0,0 +1,28 @@
+package assignment01;
+
+public class BankAccount {
+
+    private String accountNumber;
+    private double balance;
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+        }
+    }
+}
+
---
+++ src/test/java/assignment01/Assignment01Test.java
@@ -0,0 +1,62 @@
+package assignment01;
+
+
+import org.junit.Test;
+
+public class Assignment01Test {
+    @Test
+    public void BankAccountTest(){
+
+    }
+
+    @Test
+    public void AccountDetailsAccountNumberTest(){
+
+    }
+
+    @Test
+    public void AccountDetailsAccountBalanceTest(){
+
+    }
+
+    @Test
+    public void AccountDepositNegativeTest(){
+
+
+    }
+
+    @Test
+    public void AccountDepositZeroTest() {
+
+    }
+
+    @Test
+    public void AccountWithdrawalNegativeWithdrawalTest(){
+
+    }
+
+    @Test
+    public void AccountWithdrawalOverdraftTest(){
+
+    }
+
+    @Test
+    public void AccountWithdrawalLimitTest(){
+
+    }
+
+    @Test
+    public void AccountTransactionTestDeposit(){
+
+    }
+
+    @Test
+    public void AccountTransactionWithdrawalTest(){
+
+    }
+
+    @Test
+    public void AccountErrorHandlingBalanceBelowZeroTest(){
+
+    }
+}
