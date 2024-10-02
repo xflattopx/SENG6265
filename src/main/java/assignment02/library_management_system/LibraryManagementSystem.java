4ed2082799ed6f8f132f199437aff448a96f999f
Moore, Chase <moore.chase@principal.com>
2024-10-02 10:33:31 -0400

    added tests for library management system

--- src/main/java/assignment02/library_management_system/LibraryManagementSystem.java
+++ src/main/java/assignment02/library_management_system/LibraryManagementSystem.java
@@ -1,5 +1,75 @@
 package assignment02.library_management_system;
 
+import assignment02.list_management_system.ListItem;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class LibraryManagementSystem {
+
+    List<Book> books = new ArrayList<Book>();
+
+    public LibraryManagementSystem() {
+
+    }
+
+    public LibraryManagementSystem(ArrayList<Book> books){
+        this.books = books;
+    }
+
+    public void add(String title, String author) {
+        books.add(new Book(title, author));
+    }
+
+    public void remove(String title) {
+        books.remove(getIndexByTitle(title));
+    }
+
+    public String searchBook(String title) {
+
+
+        if (books.isEmpty()) {
+            return "This book isn't available"; // Return a message if the list is empty
+        }
+
+        Book book = books.get(getIndexByTitle(title));
+        StringBuilder displayBook = new StringBuilder();
+        displayBook.append(book.getTitle())
+                    .append(", ")
+                    .append(book.getAuthor());
+
+        return displayBook.toString();
+    }
+
+    public String displayLibrary(){
+        StringBuilder list = new StringBuilder();
+
+        if (books.isEmpty()) {
+            return "No books available."; // Return a message if the list is empty
+        }
+
+        for (int i = 0; i < books.size(); i++) {
+            Book book = books.get(i);
+            list.append(i + 1)  // Using i + 1 for 1-based index
+                    .append(", ")
+                    .append(book.getTitle())
+                    .append(", ")
+                    .append(book.getAuthor())
+                    .append("\n");
+        }
+
+        return list.toString();
+    }
+
+
+    private int getIndexByTitle(String task) {
+        for (int i = 0; i < books.size(); i++) {
+            if (task.equals(books.get(i).getTitle())) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
--- src/test/java/assignment02/LibraryManagementSystemTest.java
+++ src/test/java/assignment02/LibraryManagementSystemTest.java
@@ -1,21 +1,49 @@
 package assignment02;
 
+import assignment02.library_management_system.Book;
 import assignment02.library_management_system.LibraryManagementSystem;
+import assignment02.list_management_system.ListItem;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibraryManagementSystemTest {
+    LibraryManagementSystem expected;
+
+
+    @Before
+    public void setup(){
+        Book book = new Book("I Love Software Engineering", "Chase Moore");
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(book);
+        expected = new LibraryManagementSystem(books);
+    }
     @Test
     public void addBookTest(){
         LibraryManagementSystem lms = new LibraryManagementSystem();
+        lms.add("I Love Software Engineering", "Chase Moore");
+        Assert.assertEquals(expected.displayLibrary(),lms.displayLibrary());
+
     }
+
+
 
     @Test
     public void removeBookTest(){
         LibraryManagementSystem lms = new LibraryManagementSystem();
+        lms.add("I Love Software Engineering", "Chase Moore");
+        lms.remove("I Love Software Engineering");
+        Assert.assertEquals("No books available.",lms.displayLibrary());
     }
 
     @Test
     public void searchBookTest(){
         LibraryManagementSystem lms = new LibraryManagementSystem();
+        lms.add("I Love Software Engineering", "Chase Moore");
+        lms.add("I'm Burned Out", "Chase Moore");
+        Assert.assertEquals("I'm Burned Out, Chase Moore",lms.searchBook("I'm Burned Out"));
     }
 }
